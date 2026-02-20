ccc6ce86d211dd23c1336946defad4a584365e31
Pranav Bodawala <pranavkbd@gmail.com>
2026-02-20 13:56:22 -0700

    Update

--- src/routes/api/tickets.ts
+++ src/routes/api/tickets.ts
@@ -1,10 +1,11 @@
 import fp from "fastify-plugin";
-import type { FastifyPluginAsync } from "fastify";
+import type { FastifyBaseLogger } from "fastify";
 import { Type } from "@sinclair/typebox";
+import type { Static } from "@sinclair/typebox";
 import type { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
-import { tx } from "../../db/tx.js";
+import type postgres from "postgres";
 
-const CreateTicketBody = Type.Object(
+const TicketCreateBody = Type.Object(
     {
         title: Type.String({ minLength: 3, maxLength: 200 }),
         description: Type.String({ minLength: 1, maxLength: 10_000 }),
@@ -16,35 +17,30 @@ const CreateTicketBody = Type.Object(
 const routes: FastifyPluginAsyncTypebox = async (app) => {
     app.post("/tickets", {
         schema: {
-            body: CreateTicketBody,
+            body: TicketCreateBody,
         }
     }, async (req, reply) => {
+        const ticket = await createTicket(app.sql, req.body);
 
-        const createdBy = "d5e4cd76-e6a6-4794-be0d-2963dd58fe78";
-
-        try {
-            const rows = await app.sql`
-                insert into tickets (created_by, title, description, priority)
-                values (
-                    ${createdBy},
-                    ${req.body.title},
-                    ${req.body.description},
-                    ${req.body.priority ?? 3}
-                )
-                returning *;
-            `;
-
-            req.log.info({ ticket: rows[0] }, "Ticket created");
-            req.log.error("No error here")
-
-            return reply.status(201).send({ ticket: rows[0] });
-
-        } catch (err) {
-            req.log.error({ err }, "Failed to create ticket");
-            return app.httpErrors.internalServerError("Failed to create ticket");
-        }
-
+        req.log.info({ ticket }, "Ticket created");
+        return reply.status(201).send({ ticket });
     })
+}
+
+const createTicket = async (sql: postgres.Sql, data: Static<typeof TicketCreateBody>) => {
+    const createdBy = "d5e4cd76-e6a6-4794-be0d-2963dd58fe78";
+
+    return await sql`
+        insert into tickets (created_by, title, description, priority)
+        values (
+            ${createdBy},
+            ${data.title},
+            ${data.description},
+            ${data.priority ?? 3}
+        )
+        returning *;
+    `;
+
 }
 
 export default routes;
